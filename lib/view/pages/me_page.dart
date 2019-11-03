05c2ece9cb48b7c1a680bf263f02bdebc7dcc9b5
gaoguanqi <307590625@qq.com>
2019-11-03 16:03:04 +0800

    me

--- lib/view/pages/me_page.dart
+++ lib/view/pages/me_page.dart
@@ -1,11 +1,52 @@
 import 'package:flutter/material.dart';
 
-class MePage extends StatelessWidget {
+class MePage extends StatefulWidget {
+  @override
+  _MePageState createState() => _MePageState();
+}
+
+class _MePageState extends State<MePage> {
+  String _title = 'title';
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Text('me'),
+    return Scaffold(
+      appBar: AppBar(
+        title: Row(
+          children: <Widget>[
+            IconButton(
+                icon: Icon(
+                  Icons.settings,
+                  color: Colors.black45,
+                  size: 18.0,
+                ),
+                onPressed: () => onClickSetting()),
+            Text(
+              '$_title',
+              style: TextStyle(
+                  color: Colors.black45, fontSize: 16.0, letterSpacing: 2.0),
+            ),
+            IconButton(
+                icon: Icon(
+                  Icons.email,
+                  color: Colors.black45,
+                  size: 18.0,
+                ),
+                onPressed: () => onClickEmail()),
+          ],
+        ),
+        backgroundColor: Colors.white,
+        elevation: 0.5,
+      ),
+      body: Container(),
     );
+  }
+
+  onClickSetting() {
+
+  }
+
+  onClickEmail() {
 
   }
 }
