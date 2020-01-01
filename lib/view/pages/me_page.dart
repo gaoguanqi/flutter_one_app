854ba91b215b933235a3deddf3ccf798b45644da
gaoguanqi <307590625@qq.com>
2020-01-01 22:16:25 +0800

    me

--- lib/view/pages/me_page.dart
+++ lib/view/pages/me_page.dart
@@ -10,8 +10,18 @@ class MePage extends StatefulWidget {
 class _MePageState extends State<MePage> {
   String _title = 'title';
 
+  bool _hasLogin = true;
+
   @override
   Widget build(BuildContext context) {
+    return _hasLogin ? _buildUsed() : _buildKnow();
+  }
+
+  onClickSetting() {}
+
+  onClickEmail() {}
+
+  Widget _buildKnow() {
     return Scaffold(
       appBar: AppBar(
         title: Row(
@@ -25,74 +35,103 @@ class _MePageState extends State<MePage> {
                 onPressed: () => onClickSetting()),
             Expanded(
               child: Center(
-                child: Text(
-                  '$_title',
-                  style: TextStyle(
-                      color: Colors.black45,
-                      fontSize: 16.0,
-                      letterSpacing: 2.0),
+                child: Offstage(
+                  offstage: !_hasLogin,
+                  child: Text(
+                    '$_title',
+                    style: TextStyle(
+                        color: Colors.black45,
+                        fontSize: 16.0,
+                        letterSpacing: 2.0),
+                  ),
                 ),
               ),
               flex: 1,
             ),
-            IconButton(
-                icon: Icon(
-                  Icons.email,
-                  color: Colors.black45,
-                  size: 18.0,
-                ),
-                onPressed: () => onClickEmail()),
+            Offstage(
+              offstage: !_hasLogin,
+              child: IconButton(
+                  icon: Icon(
+                    Icons.email,
+                    color: Colors.black45,
+                    size: 18.0,
+                  ),
+                  onPressed: () => onClickEmail()),
+            ),
           ],
         ),
         backgroundColor: Colors.white,
         elevation: 0.5,
       ),
-      body: _buildKnow()
-    );
-  }
-
-  onClickSetting() {}
-
-  onClickEmail() {}
-
-  Widget _buildKnow() {
-    return SingleChildScrollView(
-      child: Container(
-        color: Colors.white,
+      body: Container(
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: <Widget>[
-            Image.asset(ImageHelper.wrapAssets('bg_one_logo.jpg')),
-            ClipOval(
-              child: Container(
-                color: Colors.black87,
-                width: 92.0,
-                height: 92.0,
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: <Widget>[
-                    Text(
-                      '登录',
-                      style: TextStyle(fontSize: 14.0, color: Colors.white),
+            Flexible(
+              child: Column(
+                children: <Widget>[
+                  Image.asset(ImageHelper.wrapAssets('bg_one_logo.jpg')),
+                  ClipOval(
+                    child: Container(
+                      color: Colors.black87,
+                      width: 92.0,
+                      height: 92.0,
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: <Widget>[
+                          Text(
+                            '登录',
+                            style:
+                                TextStyle(fontSize: 14.0, color: Colors.white),
+                          ),
+                          Text(
+                            'ONE一个',
+                            style: TextStyle(
+                                fontSize: 14.0,
+                                fontWeight: FontWeight.bold,
+                                color: Colors.white),
+                          ),
+                        ],
+                      ),
                     ),
-                    Text(
-                      'ONE一个',
-                      style: TextStyle(
-                          fontSize: 16.0,
-                          fontWeight: FontWeight.bold,
-                          color: Colors.white),
+                  ),
+                  Padding(padding: EdgeInsets.only(top: 20.0)),
+                  Text(
+                    '还没有账号？点击注册',
+                    style: TextStyle(
+                      fontSize: 12.0,
+                      color: Colors.black,
                     ),
-                  ],
-                ),
+                  ),
+                ],
               ),
+              flex: 1,
+            ),
+            Row(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: <Widget>[
+                Image.asset(
+                  ImageHelper.wrapAssets('ic_weichart.png'),
+                  width: 30.0,
+                ),
+                Image.asset(
+                  ImageHelper.wrapAssets('ic_sina.png'),
+                  width: 30.0,
+                ),
+                Image.asset(
+                  ImageHelper.wrapAssets('ic_qq.png'),
+                  width: 30.0,
+                ),
+              ],
             ),
             Padding(padding: EdgeInsets.only(top: 20.0)),
-            Text('还没有账号？点击注册',style: TextStyle(
-              fontSize: 12.0,
-              color: Colors.black,
-            ),),
-
-            Text('sssssss')
+            Text(
+              '创建账号即代表您同意用户服务协议',
+              style: TextStyle(fontSize: 10.0, color: Colors.black),
+            ),
+            Padding(padding: EdgeInsets.only(top: 10.0)),
           ],
         ),
       ),
@@ -100,6 +139,62 @@ class _MePageState extends State<MePage> {
   }
 
   Widget _buildUsed() {
-    return Text('已登录');
+    return Scaffold(
+      body: CustomScrollView(
+        physics: ScrollPhysics(),
+        slivers: <Widget>[
+          SliverAppBar(
+            expandedHeight: 380.0,
+            pinned: false,
+            actions: <Widget>[
+              IconButton(
+                icon: Icon(
+                  Icons.local_post_office,
+                  color: Colors.white70,
+                ),
+              ),
+            ],
+            leading: IconButton(
+              icon: Icon(
+                Icons.settings,
+                color: Colors.white70,
+              ),
+            ),
+            flexibleSpace: FlexibleSpaceBar(
+              centerTitle: true,
+              background: Stack(
+                alignment: Alignment.center,
+                children: <Widget>[
+                  Image.asset(
+                    ImageHelper.wrapAssets('bg_me_know.png'),
+                    fit: BoxFit.cover,
+                    height: 480.0,
+                  ),
+                  Positioned(
+                    top: 88.0,
+                    child: Column(
+                      children: <Widget>[
+                        InkWell(
+                          child: ClipOval(
+                              child: Image.asset(
+                                ImageHelper.wrapAssets('bg_one_logo.jpg'),
+                                fit: BoxFit.cover,
+                                width: 66.0,
+                                height: 66.0,
+                              ),
+                          ),
+                          onTap: (){
+                          },
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
